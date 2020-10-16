8f60f66cb3be0046b48b47f7f6a6904791763e21
Jeremy Lalonde <1852209@cstjean.qc.ca>
2020-10-16 12:01:45 -0400

    Jeu de base

--- devinettes.py
+++ devinettes.py
@@ -1,16 +1,41 @@
 #!/usr/bin/env python3
+import random
 """
 Jeu de devinettes.
 
 Par Jeremy Lalonde
 """
 
+
 def main():
-    """
+    nbEssais = 1
+    nombre = random.randint(1, 100)
+    print("""Bonjour, je m'apelle Jeremy 2020
+J'ai choisi un nombre entier entre 1 et 100
+Pouvez-vous le deviner?""")
+    essai = int(input(f"Essai {nbEssais}: "))
+    while essai != nombre:
+        if essai > nombre:
+            print("Votre nombre est trop grand...")
+            essai = int(input(f"Essai {nbEssais}: "))
+            nbEssais = nbEssais + 1
+        elif essai < nombre:
+            print("Votre nombre est trop petit...")
+            essai = int(input(f"Essai {nbEssais}: "))
+            nbEssais = nbEssais + 1
+        elif essai == nombre:
+            print("Bravo, vous avez deviné le nombre")
 
-    :return:
-    """
+    print("Bravo, vous avez trouvé le nombre")
 
-    if __name__ =='__main__':
+
+
+
+
+
+
+
+
+
+if __name__ =='__main__':
         main()
-            
