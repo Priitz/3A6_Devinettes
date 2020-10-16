d6473b7b604588c994199acca4ce1aee38916696
Jeremy Lalonde <1852209@cstjean.qc.ca>
2020-10-16 10:55:48 -0400

    Mise en place

---
+++ devinettes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Jeu de devinettes.
+
+Par Jeremy Lalonde
+"""
+
+def main():
+    """
+
+    :return:
+    """
+
+    if __name__ =='__main__':
+        main()
+            
